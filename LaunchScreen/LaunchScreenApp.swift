cd19e61ce010f1ee7898481a99f718b2050bc522
Julien-Bernardo <jfb2hk@gmail.com>
2024-09-16 12:27:23 +0100

    Initial Commit

---
+++ LaunchScreen/LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenApp.swift
+//  LaunchScreen
+//
+//  Created by Julien Bernardo on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
